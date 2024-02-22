607bb2b49d8c70c04c71a53c922da01a8e817f81
selmanfariz18 <salmanulfarizyka@gmail.com>
2024-02-22 11:50:20 +0530

    base templates added

--- ngo_connect/base/views.py
+++ ngo_connect/base/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home_base(request):
+    return render(request, 'base_home.html')
+
+def sighup(request):
+    return render(request, 'signup.html')

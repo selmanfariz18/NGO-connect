7f538d213d4c5aa3de7e8d59e444cf1775eacb2e
selmanfariz18 <salmanulfarizyka@gmail.com>
2024-02-24 20:44:49 +0530

    user_type model added

--- ngo_connect/base/models.py
+++ ngo_connect/base/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, User
 
 # Create your models here.
+
+class ngousers(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_type = models.CharField(max_length=10, choices=[
+        ('NGO', 'NGO'),
+        ('Donor', 'Donor'),
+        ('Receiver','Receiver'),
+    ])
+    phone_number = models.CharField(max_length=13, null=True)
+    address = models.CharField(max_length=100, null=True)
+    pincode = models.CharField(max_length=10, null=True)
+

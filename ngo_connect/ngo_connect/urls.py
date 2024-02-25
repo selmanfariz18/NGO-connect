7e600cb99976d240ac1878dcc635be38cdd0c869
selmanfariz18 <salmanulfarizyka@gmail.com>
2024-02-25 12:01:23 +0530

    Receiver signin done

--- ngo_connect/ngo_connect/urls.py
+++ ngo_connect/ngo_connect/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('base/', include('base.urls')),
     path('ngo/', include('ngo.urls')),
     path('donor/',include('donor.urls')),
+    path('receiver', include('reciever.urls')),
 ]

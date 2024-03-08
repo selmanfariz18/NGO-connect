b0c3745679738be2901745308c340633c629a4a3
selmanfariz18 <salmanulfarizyka@gmail.com>
2024-03-08 22:26:05 +0530

    counter error try false

--- ngo_connect/ngo/views.py
+++ ngo_connect/ngo/views.py
@@ -18,11 +18,13 @@ def ngo_base(request):
     user_name=request.user
     users = User.objects.all()
     user_details = ngousers.objects.all()
+    count = 1
 
     context = {
         'user' : user_name,
         'users' : users,
         'user_details' : user_details,
+        'count' : count,
     }
 
     return render(request, 'ngo_base.html', context)

755d18407c9bf57ccdefff8319d864dd6ac9aafe
selmanfariz18 <salmanulfarizyka@gmail.com>
2024-03-27 20:12:45 +0530

    model for bank and reciever-ngo conn created

--- ngo_connect/ngo/models.py
+++ ngo_connect/ngo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, User
+
 
 # Create your models here.
+
+class NgoBank(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    current_balance = models.IntegerField(null=True)
+    last_transaction = models.CharField(max_length=255, null=True)
+    transaction_log = models.JSONField(null=True)
--- ngo_connect/reciever/models.py
+++ ngo_connect/reciever/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, User
+from django.conf import settings
 
-# Create your models here.
+class ReceiverMoreDetails(models.Model):
+    reciever_type = models.CharField(max_length=12, null=True)  # Whether receiver is OLH, Orphanage, etc.
+    registered_ngo = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.SET_NULL, 
+        null=True, 
+        blank=True,  # This allows the field to be blank in forms as well.
+        related_name='registered_ngos'  # This is optional but recommended for reverse querying.
+    )
+class RecieverBank(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    current_balance = models.IntegerField(null=True)
+    last_transaction = models.CharField(max_length=255, null=True)
+    transaction_log = models.JSONField(null=True)

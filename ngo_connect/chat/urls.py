5f0dedffa72d7c4be4348e628c0326286da1652e
selmanfariz18 <salmanulfarizyka@gmail.com>
2024-04-27 18:22:58 +0530

    chat basic idea works

--- ngo_connect/chat/urls.py
+++ ngo_connect/chat/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from chat import views
+from chat.views import SendMessageView, ChatView, ChatWithUserView
 
 urlpatterns = [
     path('',views.home_chat, name="home_chat"),
+    path('send/', SendMessageView.as_view(), name='send_message'),
+    path('chat/', ChatView.as_view(), name='chat'),
+    path('<str:username>/', ChatWithUserView.as_view(), name='chat_with'),
 ]
--- ngo_connect/chat/views.py
+++ ngo_connect/chat/views.py
@@ -5,9 +5,46 @@ from base.models import ngousers, Notifications
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect
+from chat.models import Message
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic import View
+from django.db.models import Q
 
 # Create your views here.
 
 
 def home_chat(request):
-    return render(request, 'home_chat.html')
+    return render(request, 'chat.html')
+
+class SendMessageView(View):
+    @method_decorator(login_required)
+    def post(self, request, *args, **kwargs):
+        receiver_username = request.POST.get('receiver')
+        message_text = request.POST.get('message')
+        receiver = User.objects.get(username=receiver_username)
+        Message.objects.create(sender=request.user, receiver=receiver, message=message_text)
+        return redirect('chat')
+
+class ChatView(View):
+    @method_decorator(login_required)
+    def get(self, request, *args, **kwargs):
+        # Fetch messages where the user is either the sender or the receiver
+        messages = Message.objects.filter(Q(sender=request.user) | Q(receiver=request.user)).order_by('-timestamp')
+        
+        # Get distinct users involved in the messages
+        users_messaged = User.objects.filter(
+            Q(sent_messages__receiver=request.user) | Q(received_messages__sender=request.user)
+        ).distinct()
+
+        return render(request, 'chat.html', {'messages': messages, 'users': users_messaged})
+    
+class ChatWithUserView(View):
+    @method_decorator(login_required)
+    def get(self, request, username, *args, **kwargs):
+        other_user = User.objects.get(username=username)
+        messages = Message.objects.filter(
+            (Q(sender=request.user) & Q(receiver=other_user)) | 
+            (Q(sender=other_user) & Q(receiver=request.user))
+        ).order_by('timestamp')
+        return render(request, 'chat_with_user.html', {'messages': messages, 'other_user': other_user})

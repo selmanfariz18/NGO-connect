0204ac71cc1c2e60bd3506fb9cdca45288286d24
selmanfariz18 <salmanulfarizyka@gmail.com>
2024-05-17 22:41:15 +0530

    browser confirmation added in logout on base pages

--- ngo_connect/ngo_connect/settings.py
+++ ngo_connect/ngo_connect/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-v+#yn!=lm0wg*#jar2iin+uxeu+b(nqrsq%#%jcunqk_ingf5)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
